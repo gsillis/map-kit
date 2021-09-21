484cf2c195346770a5d1501a93936196bc03ce9e
Gabriela Sillis <gabydias47@gmail.com>
2021-09-21 20:41:05 -0300

    decode geojson into array data

--- Mapa/Mapa/Artwork.swift
+++ Mapa/Mapa/Artwork.swift
@@ -8,6 +8,9 @@
 import Foundation
 import MapKit
 
+// it's a framework using to set the address, city or state fields of a location
+import Contacts
+
 // MKAnnotation is an NSObjectProtocol
 class Artwork: NSObject, MKAnnotation {
     let title: String?
@@ -26,7 +29,38 @@ class Artwork: NSObject, MKAnnotation {
         super.init()
     }
 
+    init?(feature: MKGeoJSONFeature) {
+        guard
+            let point = feature.geometry.first as? MKPointAnnotation,
+            let propertiesData = feature.properties,
+            let json = try? JSONSerialization.jsonObject(with: propertiesData),
+            let properties = json as? [String: Any]
+        else {
+            return nil
+        }
+
+        title = properties["title"] as? String
+        locationName = properties["location"] as? String
+        discipline = properties["discipline"] as? String
+        coordinate = point.coordinate
+
+        super.init()
+    }
+
     var subtitle: String?  {
         return locationName
+    }
+
+    var mapItem: MKMapItem? {
+        guard let location = locationName else {
+            return nil
+        }
+
+        let addressDict = [CNPostalAddressStateKey: location]
+        let placemark = MKPlacemark(coordinate: coordinate, addressDictionary: addressDict)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = title
+
+        return mapItem
     }
 }
--- Mapa/Mapa/ViewController.swift
+++ Mapa/Mapa/ViewController.swift
@@ -12,12 +12,34 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
 
+    private var artworks: [Artwork] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
         mapView.centerToLocation(location: initLocation())
         configureMapZoom()
-        configureArtwork()
+        loadInitialData()
+        mapView.addAnnotations(artworks)
+    }
+
+    private func loadInitialData() {
+        guard
+            let fileName = Bundle.main.url(forResource: "PublicArt", withExtension: "geojson"),
+            let artworkData = try? Data(contentsOf: fileName)
+        else {
+            return
+        }
+
+        do {
+            let features = try MKGeoJSONDecoder()
+                .decode(artworkData)
+                .compactMap {$0 as? MKGeoJSONFeature}
+            let validWorks = features.compactMap(Artwork.init)
+            artworks.append(contentsOf: validWorks)
+        } catch  {
+            print("Unexpected error: \(error).")
+        }
     }
 
     private func initLocation() -> CLLocation {
@@ -32,13 +54,8 @@ class ViewController: UIViewController {
         mapView.setCameraBoundary(MKMapView.CameraBoundary.init(coordinateRegion: region), animated: true)
 
         let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
+        
         mapView.setCameraZoomRange(zoomRange, animated: true)
-    }
-
-    private func configureArtwork() {
-        let artwork = Artwork(title: "King David Kalakaua", locationName: "Waikiki Gateway Park", discipline: "Sculpture", coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
-
-        mapView.addAnnotation(artwork)
     }
 }
 
@@ -46,7 +63,9 @@ private extension MKMapView {
 
     func centerToLocation(location: CLLocation, regionRadius: CLLocationDistance = 1000) {
 
-        let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                                  latitudinalMeters: regionRadius,
+                                                  longitudinalMeters: regionRadius)
 
         setRegion(coordinateRegion, animated: true)
     }
@@ -73,6 +92,14 @@ extension ViewController: MKMapViewDelegate {
             view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
         }
         return view
+    }
+
+    // open the map if user taps info button
+    func mapView( _ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let artwork = view.annotation as? Artwork else { return }
+
+        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+        artwork.mapItem?.openInMaps(launchOptions: launchOptions)
     }
 }
 
